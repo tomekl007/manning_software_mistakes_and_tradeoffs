a9efdf1a070c3e7f058ea0aae0084e14a7eb1fbd
tomekl007 <tomasz.lelek@datastax.com>
2020-09-26 09:34:26 +0200

    init commit

---
+++ src/main/java/com/tomekl007/App.java
@@ -0,0 +1,13 @@
+package com.tomekl007;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tomekl007/AppTest.java
@@ -0,0 +1,20 @@
+package com.tomekl007;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
