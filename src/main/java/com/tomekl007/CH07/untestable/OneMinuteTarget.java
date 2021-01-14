86f2d39e831240c8d3164815758f21dd83a70080
Jon Skeet <skeet@pobox.com>
2021-01-14 09:02:08 +0000

    Make the target non-null

--- src/main/java/com/tomekl007/CH07/untestable/OneMinuteTarget.java
+++ src/main/java/com/tomekl007/CH07/untestable/OneMinuteTarget.java
@@ -3,12 +3,14 @@ package com.tomekl007.CH07.untestable;
 import java.time.Duration;
 import java.time.Instant;
 
+import javax.annotation.Nonnull;
+
 public final class OneMinuteTarget {
   private static final Duration ONE_MINUTE = Duration.ofMinutes(1);
   private final Instant minInclusive;
   private final Instant maxInclusive;
 
-  public OneMinuteTarget(Instant target) {
+  public OneMinuteTarget(@Nonnull Instant target) {
     minInclusive = target.minus(ONE_MINUTE);
     maxInclusive = target.plus(ONE_MINUTE);
   }
