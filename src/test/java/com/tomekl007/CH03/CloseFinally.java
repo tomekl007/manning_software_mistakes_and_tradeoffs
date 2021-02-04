3f986d441b205336abd9cafb827bb71c00b12f14
tomekl007 <tomasz.lelek@datastax.com>
2021-02-04 17:27:46 +0100

    add close finally example.

---
+++ src/test/java/com/tomekl007/CH03/CloseFinally.java
@@ -0,0 +1,31 @@
+package com.tomekl007.CH03;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.jupiter.api.Test;
+
+public class CloseFinally {
+
+  @Test
+  public void shouldCloseClient() {
+    assertThatThrownBy(
+            () -> {
+              CloseableHttpClient client = HttpClients.createDefault();
+              try {
+                processRequests(client);
+              } finally {
+                System.out.println("closing");
+                client.close();
+              }
+            })
+        .isInstanceOf(UncheckedIOException.class);
+  }
+
+  private void processRequests(CloseableHttpClient client) throws UncheckedIOException {
+    throw new UncheckedIOException(new IOException("error"));
+  }
+}
