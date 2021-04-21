7e0c300334bddf5b588bac7ea6280084db3698ba
tomekl007 <tomasz.lelek@datastax.com>
2021-04-21 17:05:18 +0200

    removing not-needed test

--- src/test/java/com/tomekl007/CH11/producer/SpringKafkaSenderTest.java
+++ src/test/java/com/tomekl007/CH11/producer/SpringKafkaSenderTest.java
@@ -1,18 +1,15 @@
 package com.tomekl007.CH11.producer;
 
-import static com.tomekl007.CH11.AllSpringKafkaTests.NUMBER_OF_PARTITIONS_PER_TOPIC;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.kafka.test.assertj.KafkaConditions.key;
 import static org.springframework.kafka.test.assertj.KafkaConditions.value;
 
 import com.tomekl007.CH11.AllSpringKafkaTests;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -107,22 +104,5 @@ public class SpringKafkaSenderTest {
 
     // then
     assertThat(records.poll(10, TimeUnit.SECONDS)).has(value(content)).has(key(userId));
-  }
-
-  @Test
-  public void
-      givenMessage_whenSendForOtherThanSpecificPartitionKey_thenMessageShouldAlwaysLandNOTInLastPartition()
-          throws Exception {
-    // given
-    String content = "User viewed page C";
-    Integer userId = new Random().nextInt(100_000);
-
-    // when
-    RecordMetadata recordMetadata =
-        sender.sendBlocking(AllSpringKafkaTests.SENDER_TOPIC, content, userId);
-
-    // then
-    assertThat(records.poll(10, TimeUnit.SECONDS)).has(value(content)).has(key(userId));
-    assertThat(recordMetadata.partition()).isBetween(0, NUMBER_OF_PARTITIONS_PER_TOPIC - 2);
   }
 }
