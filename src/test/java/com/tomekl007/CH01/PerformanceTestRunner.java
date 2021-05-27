5ea83fb5af27246a4031e67275f4ed0d98669f18
tomekl007 <tomasz.lelek@datastax.com>
2021-05-27 12:16:12 +0200

    rename

--- src/test/java/com/tomekl007/CH01/PerformanceTestRunner.java
+++ src/test/java/com/tomekl007/CH01/PerformanceTestRunner.java
@@ -4,7 +4,7 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-public class ExceptionsPerformanceTestRunner {
+public class PerformanceTestRunner {
   public static void main(String[] args) throws Exception {
     Options opt =
         new OptionsBuilder().include(BenchmarkSingletonVsThreadLocal.class.getSimpleName()).build();
